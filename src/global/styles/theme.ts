a457dbbd1947632c507581900e0a0168b24dc65a
João Victor <joaovr.costa@gmail.com>
2023-05-20 11:37:42 -0300

    att theme colors

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,22 +1,14 @@
 export default {
   colors: {
-    primary: "#5636D3",
-
-    secondary: "#FF872C",
-    secondary_light: "rgba(255, 135, 44, 0.3)",
-
-    success: "#12A454",
-    success_light: "rgba(18, 164, 84, 0.5)",
-
-    attention: "#E83F5B",
-    attention_light: "rgba(232, 63, 91, 0.5)",
-
-    shape: "#FFFFFF",
-    background: "#F0F2F5",
-
-    title: "#363F5F",
-    text: "#969CB2",
-    text_dark: "#000000",
+    primary: "#FDFFF4",
+    white: "#FFFFFF",
+    blue_300: "#82C3FF",
+    blue_500: "#45A6FF",
+    blue_950: "#10162F",
+    yellow_300: "#F7FFD2",
+    yellow_400: "#FFF0BB",
+    yellow_500: "#FFD644",
+    purple_500: "#A22CFF",
   },
   fonts: {
     regular: "Poppins_400Regular",
