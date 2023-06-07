6bdcdabad48d9ab17c5ab42a37277e9da53818ef
Raphael_Pauleschi <Raphael.f.pauleschi@gmail.com>
2023-06-07 10:46:08 -0300

    Developing: tab navigation

---
+++ src/screens/QuestionCreationPage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import{View,Text} from "react-native"
+  
+  export function QuestionCreationPage() {
+    return (
+      <View>
+        <Text>Eu sou uma página de teste, por favor coloque conteúdo</Text>
+      </View>
+    );
+  }
+  
---
+++ src/screens/QuestionCreationPage/styles.ts
@@ -0,0 +1,2 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
